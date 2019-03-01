5ea4a285afdb199a8aa2ceaaa784af0bc8774964
Glitch (exercise-playlist) <none>
2019-03-01 10:17:03 +0000

    🌆😽 Checkpoint ./controllers/dashboard.js:2922553/1952 ./controller/playlist.js:2922553/1022 ./views/playlist.hbs:2922553/598 ./models/playlist-store.js:2922553/561 ./controllers/playlist.js:2922553/888 ./routes.js:2922553/1

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const start = require('./controllers/start');
 const dashboard = require('./controllers/dashboard.js');
 const about = require('./controllers/about.js');
-const playlist = require('./controller/playlist.js');
+const playlist = require('./controllers/playlist.js');
 
 router.get('/', start.index);
 router.get('/dashboard', dashboard.index);
