da11a50c358dd286d881375562d2b255c77254cb
Glitch (exercise-playlist) <none>
2019-03-01 10:49:04 +0000

    🍖🏤 Checkpoint ./models/playlist-store.js:2922553/1595 ./models/playlist-store.json:2922553/7420 ./views/dashboard.hbs:2922553/1946

--- models/playlist-store.js
+++ models/playlist-store.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const _ = require('lodash');
 const playlistStore = {
 
   playlistCollection: require('./playlist-store.json').playlistCollection,
@@ -8,19 +8,12 @@ const playlistStore = {
     return this.playlistCollection;
   },
 
-  getPlaylist(id) {
-    let foundPlaylist = null;
-    for (let playlist of this.playlistCollection) {
-      if (id == playlist.id) {
-        foundPlaylist = playlist;
-      }
-    }
-
-    return foundPlaylist;
+   getPlaylist(id) {
+    return _.find(this.playlistCollection, { id: id });
   },
    removeSong(id, songId) {
     const playlist = this.getPlaylist(id);
-    // remove the song with id songId from the playlist
+    _.remove(playlist.songs, { id: songId });
   },
 };
 
