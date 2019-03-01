a362d1c4e46670aea1ebef26411bf4ab8bca9005
Glitch (exercise-playlist) <none>
2019-03-01 09:56:02 +0000

    🤗💊 Checkpoint ./routes.js:2922553/265 ./controller/playlist.js:2922553/551 ./views/playlist.hbs:2922553/155

--- routes.js
+++ routes.js
@@ -5,18 +5,10 @@ const router = express.Router();
 
 const start = require('./controllers/start');
 const dashboard = require('./controllers/dashboard.js');
-
+const about = require('./controllers/about.js');
 
 router.get('/', start.index);
 router.get('/dashboard', dashboard.index);
 router.get('/about', about.index);
 
 module.exports = router;
-const about = {
-  index(request, response) {
-    const viewData ={
-      title: ' About PlaylistMaker',
-    };
-    response.render('about',viewData);
-  },
-};
